dc889911e43ec5abe46f12c43bbb7432663aa24c
Filippo Utro <futro@us.ibm.com>
2026-06-04 14:23:25 -0400

    home

--- pages/1_Gironi.py
+++ pages/1_Gironi.py
@@ -205,9 +205,9 @@ with col_center:
     st.image("Mundial&Me Logo Nero.png", use_container_width=True)
     
     # Questo è il modo più pulito: un bottone "Home" che richiama la funzione
-    if st.button("🏠 Home", use_container_width=True, on_click=reset_al_main):
-        pass
-
+    if st.button("🏠 Home", use_container_width=True):
+        reset_al_main()
+        
 # Separatore discreto
 st.markdown("<hr style='border: 0.5px solid #e0e0e0; margin: 15px 0;'>", unsafe_allow_html=True)
 # --- LOGIN UTENTE ---
