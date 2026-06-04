7731c79cdbf96cfd43106fd90d8bf0c327920ace
Filippo Utro <futro@us.ibm.com>
2026-06-04 14:41:22 -0400

    favicon

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Mundial&Me  - Home 🏆",
-    page_icon="⚽",
+    page_icon="favicon.png",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
