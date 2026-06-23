26c4023d7fbf685f159115a2d55aca54c31614d1
Filippo Utro <futro@us.ibm.com>
2026-06-23 18:02:54 -0400

    starting from Giornata 2

--- pages/2_Schedina_Live.py
+++ pages/2_Schedina_Live.py
@@ -819,7 +819,7 @@ if not giornate_disponibili:
 giornata_scelta = st.selectbox(
     "📅 Seleziona il turno:", 
     options=giornate_disponibili,
-    index=0 
+    index=1 
 )
 st.session_state.giornata = giornata_scelta
 
